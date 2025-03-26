2d6e92f546a7a1c63a2edb8bec90595c80a8772a
SoftySylph <progaming5533@gmail.com>
2025-03-26 13:10:08 -0400

    Push 1

---
+++ Python/Modules/earthquake.py
@@ -0,0 +1,8 @@
+import requests
+import time
+
+def checkSeismicActivity():
+
+    print("Checked")
+    time.sleep(1)
+    checkSeismicActivity()
---
+++ Python/main.py
@@ -0,0 +1,8 @@
+import os
+import json
+
+def main():
+    print()
+
+if __name__ == '__main__':
+    main()
